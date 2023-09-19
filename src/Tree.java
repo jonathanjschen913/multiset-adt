3f7de2eed8880e2d1920cca18edb7913a9ad46ad
cynthia <cyn.zhou@mail.utoronto.ca>
2023-09-18 20:29:12 -0400

    to string func

--- src/Tree.java
+++ src/Tree.java
@@ -1,2 +1,26 @@
 public class Tree {
+    Tree root;
+    Tree subtrees;
+
+    private static String str(){
+        return Tree.str_indented();
+    }
+
+    private static String str_indented(Integer depth){
+        if (Tree.is_empty()){
+            return "";
+        }
+        else{
+            StringBuilder s = new StringBuilder((" " * depth) + str(Tree.root) + '\n');
+            for (Tree.root: subtree){
+                s.append(subtree.str_indented(depth + 1));
+            }
+            return s.toString();
+        }
+    }
+
+    private static String str_indented(){
+        return "";
+    }
 }
+
